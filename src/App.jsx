caa7960534c39a7af455e2edd9fcac1d45bb25c9
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2023-06-21 20:04:55 -0300

    criei a cont handleTaskClick

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,17 @@ const App = () => {
     },
   ]);
 
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) return {...task, completed: !task.completed}
+
+      return task;
+    });
+    setTasks(newTasks)
+  };
+
+
   const handleTaskAddition = (taskTitle) => {
       const newTasks = [ ... tasks, {
       title: taskTitle, 
@@ -37,7 +48,7 @@ const App = () => {
     <>
       <div className='container'>
         <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
       </div>
     </>
   );
